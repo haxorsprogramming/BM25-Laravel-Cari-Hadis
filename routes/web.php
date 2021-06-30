4194b7f70b6ad57bc6c2cfcae54ae7babb5bbc24
Haxorsprogramming <haxors@uinsu.ac.id>
2021-06-30 23:23:21 +0700

    'update'

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\C_Login;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [C_Login::class, 'login_page']);
